8a90fe244bf579690b5bd8939f20ea5f39d7c18f
Karthik Batta <abhiprince4147@gmail.com>
2025-05-28 15:09:30 +0530

    Time: 25 ms (69.64%), Space: 30 MB (71.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,7 @@
 #from collections import Counter
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # Method 1
         # k=Counter(nums)
         # max_ele=None
         # m=float('-inf')
@@ -9,8 +10,25 @@ class Solution:
         #         m=freq
         #         max_ele=ele
         # return max_ele
-        k=set()
-        for ele in nums:
-            if ele in k:
-                return ele
-            k.add(ele)
+
+        #Method 2
+        # k=set()
+        # for ele in nums:
+        #     if ele in k:
+        #         return ele
+        #     k.add(ele)
+
+        # method 1 & 2 both takes the time complexity of O(N) and space complexity of O(N)
+
+        # Method 3 (Optimal with time and space complexity of O(N) and O(1))
+        slow=fast=nums[0]
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+            if slow==fast:
+                break
+        slow=nums[0]
+        while(slow!=fast):
+            slow=nums[slow]
+            fast=nums[fast]
+        return slow
